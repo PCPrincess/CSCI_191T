0c9e0af3e57812af2d761b773ed66689b6250a58
Lori Overholtzer <PCPrincess@mail.fresnostate.edu>
2015-09-14 19:36:46 -0700

    Pragmatic Programmer Chap. 3

---
+++ myStuff/pragmaticprg.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Contact struct {
+	name string
+	address string
+	phone string
+}
+
+func main() {
+	var dataBase [5]Contact
+
+	var a Contact
+	//var b Contact
+	//var c Contact
+
+	var d = Contact{name: "Lori", address: "555 West Ave"}
+
+	a.name = "Jessie"
+	a.address = "1010 Park"
+	a.phone = "555-5555"
+
+	dataBase[0] = a
+	dataBase[1] = d
+	fmt.Println(dataBase[0].name)
+
+}
+
+
+
