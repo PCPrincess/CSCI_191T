b8579bb6de228244c57e07f9de023bf7f36ceea5
PCPrincess <PCPrincess@mail.fresnostate.edu>
2015-09-24 11:45:00 -0700

    Start - Presentation14

---
+++ presentation15/types2Struct.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main()  {
+	type customer struct {
+		name string
+		ident int
+	}
+
+	cust1 := customer{"David", 001}
+	cust2 := customer{"Sarah", 002}
+
+	fmt.Println(cust1.name)
+	fmt.Println(cust1.ident)
+
+	cust2.name = "Sally"
+	fmt.Println(cust2.name)
+
+	cust3 := new(customer) //
+	cust3.name = "Bernie" //
+	fmt.Println(&cust3.name)
+
+}
